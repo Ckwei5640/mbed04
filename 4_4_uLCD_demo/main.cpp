8a94ea4d320ff1ab0ce8c447773bd8ff5ed9f9b5
Ckwei <kkww0259@gmail.com>
2020-03-31 16:23:38 +0800

    add main.cpp

--- 4_4_uLCD_demo/main.cpp
+++ 4_4_uLCD_demo/main.cpp
@@ -8,16 +8,23 @@ uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 int main()
 {
     // basic printf demo = 16 by 18 characters on screen
-    uLCD.color(GREEN);
-    uLCD.printf("\nHello uLCD World\n"); //Default Green on black text
-    uLCD.printf("\n  Starting Demo...");
+    uLCD.background_color(WHITE);
+    
+    uLCD.color(BLUE);
+    uLCD.printf("\nMy student ID :\n\n"); //Default Green on black text
+    uLCD.text_width(2); //2X size text
+    uLCD.text_height(2);
+    uLCD.printf("107061213");
     uLCD.text_width(4); //4X size text
     uLCD.text_height(4);
-    uLCD.color(RED);
+    uLCD.color(GREEN);
     
-    for (int i=10; i>=0; --i) {
+    for (int i=30; i>=0; --i) {
         uLCD.locate(1,2);
         uLCD.printf("%2D",i);
         wait(1);
     }
 }
+//Show your student ID number in blue, 
+//a 30 second count down in green,
+// and a white background on uLCD-144G2-AR.
